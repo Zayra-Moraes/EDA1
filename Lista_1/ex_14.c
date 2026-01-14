f26ffdd98a68841b5629986a18487e17b52a3596
Zayra Moraes <zayramoraes1212@gmail.com>
2026-01-14 11:13:04 -0300

    ex 14

--- Lista_1/ex_14.c
+++ Lista_1/ex_14.c
@@ -1,11 +1,31 @@
 #include <stdio.h>
+#include <string.h>
 
 typedef struct tipoBanda{
     char nome[80];
-    int qtd;
-    
-}
+    int ano;
+}tipoBanda;
 
-void pesquisarNomeMusica(char pesquisa[80], tipoBanda shows[50], int n);{
+
+typedef struct tipoShow{
+    char nome[80];
+    int qtd;
+    tipoBanda bandas[100];
+} tipoShow;
+
+void pesquisarNomeBanda(char pesquisa[80], tipoShow shows[50], int n){
+    int achou=0;
+    for(int i=0; i<n; i++){
+        for(int j=0;j<shows[i].qtd;j++){
+            if(strcmp(pesquisa,shows[i].bandas[j].nome) == 0){
+                printf("%s : ano %d\n", shows[i].nome, shows[i].bandas[j].ano);
+                achou=1;
+            }
+        }
+        
+    }
+    if(achou==0){
+        printf("Banda nao cadastrada\n");
+    }
 
 }

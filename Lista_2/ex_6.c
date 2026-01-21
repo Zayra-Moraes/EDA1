445d7f0045de80ce98b9f3e9e23383dc652e20dc
Zayra Moraes <zayramoraes1212@gmail.com>
2026-01-21 11:16:50 -0300

    falta uma questao

--- Lista_2/ex_6.c
+++ Lista_2/ex_6.c
@@ -1,13 +1,24 @@
 #include <stdio.h>
 
 int pa_recursiva (int *v, int primeiro, int ultimo){
-    
+    if(ultimo-primeiro ==1) return 1;
+
+    int soma_atual=v[primeiro]+v[ultimo];
+    int proxima_soma=v[primeiro+1]+v[ultimo-1];
+    if(soma_atual != proxima_soma) return 0;
+
+    pa_recursiva(v, primeiro+1, ultimo-1);
 }
 
-int main(){
-    int v[4]={2,4,6,8};
-    pa_recursiva(v,v[0],v[3]);
-    printf();
-    return 0;
+// int main(){
+//     int v[6]={2,5,8,11,14,17};
+//     int resultado=pa_recursiva(v,0,5);
+//     if(resultado == 1){
+//         printf("eh uma pa!\n");
+//     }
+//     else{
+//         printf("NAO eh uma pa!\n");
+//     }
+//     return 0;
 
-}
+// }
